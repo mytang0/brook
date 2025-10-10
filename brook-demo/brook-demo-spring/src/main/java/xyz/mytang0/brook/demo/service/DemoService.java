9c63ef5e73dfd6b6c576b589f86ec7a5bf09c82d
mytang0 <mytang0@apache.org>
2025-10-10 11:16:56 +0800

    Resolve the SpringBeanMethodTask method call with the same name

--- brook-demo/brook-demo-spring/src/main/java/xyz/mytang0/brook/demo/service/DemoService.java
+++ brook-demo/brook-demo-spring/src/main/java/xyz/mytang0/brook/demo/service/DemoService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-@Component
+@Component("demoService")
 public class DemoService {
 
     @Taskable(type = "test-void", description = "for testing")
